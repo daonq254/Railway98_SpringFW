b9093cb2188ab6b5e2b42d44d44f0213f70e2eaa
daonq254 <dao.nq254@gmail.com>
2026-03-24 21:32:01 +0700

    1

---
+++ R98_Lab1_HelloWorld/src/main/java/com/vti/form/AccountFormForCreating.java
@@ -0,0 +1,36 @@
+package com.vti.form;
+
+public class AccountFormForCreating {
+	private String user;
+	private String email;
+	private String fullname;
+
+	public AccountFormForCreating() {
+		super();
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFullname() {
+		return fullname;
+	}
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+}
---
+++ R98_Lab3_AccountManagement/src/main/java/com/vti/dto/DepartmentDto.java
@@ -0,0 +1,19 @@
+package com.vti.dto;
+
+public class DepartmentDto {
+	private String name;
+
+	public DepartmentDto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
